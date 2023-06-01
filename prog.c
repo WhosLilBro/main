37e8236b48e91e8ac0185ea0459117bad5a15154
Ivan <skuratov1408@mail.ru>
2023-06-01 18:44:56 -0400

    New izmen

--- prog.c
+++ prog.c
@@ -1,11 +1,2 @@
-#include<stdio.h>
-int main(
-{
-	int a =5;
-	int b =2;
-	int sum = a + b;
-	int difference = a -b;
-	printf("Сумма: %i", sum);
-	printf("Разность: %i", difference);
-	return 0;
-}
+New izmenenya
+Skuratov
